5c52ee14364f518de344bd7ef3976295faac3e97
EduMLAlmeida <edualmeidawebdev@gmail.com>
2022-02-18 17:06:51 -0300

    Atende aos requisitos 3 e 4.

---
+++ src/components/PlanetCard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class PlanetCards extends React.Component {
+  render() {
+    return (
+      <div data-testid="planet-card">
+        {/* <p data-testid="planet-name">{this.props.planetName}</p>
+        <img data-testid="planet-image"
+        src={this.props.planetImage} alt="Planeta {planetName}"></img> */}
+      </div>
+    );
+  }
+}
+
+export default PlanetCards;
